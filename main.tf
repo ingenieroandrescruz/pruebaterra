0f5cc374e7acda5d8fd9ac348fcd295997786bbf
ingenieroandrescruz <ingenieroandrescruz@gmail.com>
2026-06-24 15:55:33 -0500

    cambio de nombre stg

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "rg" {
 module "storage" {
   source = "./modules/storage-account"
 
-  storage_account_name = "mystgcicdbext"
+  storage_account_name = "mystgcicdbextgit"
   resource_group_name  = azurerm_resource_group.rg.name
   location             = azurerm_resource_group.rg.location
 }
