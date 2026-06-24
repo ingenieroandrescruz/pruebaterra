23f4cdbcdbab65024e4bf3c961cd59184c7ca89c
ingenieroandrescruz <ingenieroandrescruz@gmail.com>
2026-06-24 15:52:42 -0500

    identación backend.tf

--- backenf.tf
+++ backenf.tf
@@ -1,5 +1,5 @@
 terraform {
-backend "azurerm" {
+  backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
     storage_account_name = "mystgcicdbextstate"
     container_name       = "tfstate"
