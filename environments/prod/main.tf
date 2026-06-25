c173980788038091ab0f757481033211ae353de4
ingenieroandrescruz <ingenieroandrescruz@gmail.com>
2026-06-25 08:49:37 -0500

    paso  a ambientes separados

---
+++ environments/prod/main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    Environment = var.environment
+    Owner       = var.owner
+  }
+}
+
+module "storage" {
+  source = "../../modules/storage-account"
+
+  storage_account_name = var.storage_account_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  location             = azurerm_resource_group.rg.location
+}
